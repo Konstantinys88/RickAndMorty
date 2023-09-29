9467ddae2495a3e24fbf67059ef2dc6860f376ae
Maschkov Konstantin <maschkovc@yandex.ru>
2023-09-29 13:07:35 +0300

    настройка функционала компонента RandomChar

--- src/components/randomCharacter/RandomCharacter.js
+++ src/components/randomCharacter/RandomCharacter.js
@@ -1,33 +1,56 @@
 import './randomCharacter.scss';
 import '../../style/button.scss';
+import { useState, useEffect } from 'react';
 
-import morty from '../../resources/morty.jpeg';
+import GetFromBD from '../../services/GetFromBD';
 import rickAndMorty from '../../resources/pngwing.com.png';
 
 
-
-
-
-
 const RandomCharacter = () => {
-    return (
-        <div className='randomCharacter'>
+
+    const [char, setChar] = useState({});
+    const { getCharacters } = GetFromBD();
+
+    useEffect(() => {
+        updateChar();
+        console.log('useEffect')
+    }, []);
+
+    const onCharLoaded = (char) => {
+        setChar(char);
+    }
+
+    const updateChar = () => {
+        const id = Math.floor(Math.random() * (826 - 1)) + 1;
+        getCharacters(id).then(onCharLoaded);
+    }
+
+    const View = ({ char }) => {
+        const { name, origin, gender, species, status, image } = char;
+
+        return (
             <div className='randomCharacter__info'>
                 <div className='randomCharacter__img'>
-                    <img className='randomCharacter__info-img' src={morty} alt="morty" />
+                    <img className='randomCharacter__info-img' src={image} alt="morty" />
                 </div>
                 <div className="randomCharacter__descr">
-                    <p className='randomCharacter__descr-text'>Name: Morty Smith</p>
-                    <p className='randomCharacter__descr-text'>Origin: Unknown</p>
-                    <p className='randomCharacter__descr-text'>Gender: Male</p>
-                    <p className='randomCharacter__descr-text'>Species: Human</p>
-                    <p className='randomCharacter__descr-text'>status: Alive</p>
+                    <p className='randomCharacter__descr-text'>Name: {name}</p>
+                    <p className='randomCharacter__descr-text'>Origin: {origin}</p>
+                    <p className='randomCharacter__descr-text'>Gender: {gender}</p>
+                    <p className='randomCharacter__descr-text'>Species: {species}</p>
+                    <p className='randomCharacter__descr-text'>Status: {status}</p>
                 </div>
             </div>
+        )
+    }
+
+    return (
+        <div className='randomCharacter'>
+            <View char={char} />
             <div className="randomCharacter__tryIt">
                 <p className="randomCharacter__tryIt-text">Random character for today! <br /> Do you want to get to know him better?</p>
                 <p className="randomCharacter__tryIt-text">Or choose another one</p>
-                <button className='button randomCharacter__button'>Try it</button>
+                <button onClick={updateChar} className='button randomCharacter__button'>Try it</button>
                 <div className='randomCharacter__img'>
                     <img className='randomCharacter__tryIt-img' src={rickAndMorty} alt="rickAndMorty" />
                 </div>
--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/app/App';
 
-import GetFromBD from './services/GetFromBD';
+// import GetFromBD from './services/GetFromBD';
 
 import './style/style.scss';
 
-const { getAllCharcters, getCharacters } = GetFromBD();
+// const { getAllCharcters, getCharacters } = GetFromBD();
 
 // /test
 
-const arrayCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 826];
-getAllCharcters(arrayCharacters).then(res => console.log(res))
+// const arrayCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 826];
+// getAllCharcters(arrayCharacters).then(res => console.log(res))
 
-const num = 1;
-getCharacters(num).then(res => {
-  console.log(res)
-});
+// const num = 1;
+// getCharacters(num).then(res => {
+//   console.log(res)
+// });
 
 ///test
 
