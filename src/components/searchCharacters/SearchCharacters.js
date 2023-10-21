3c8ed9f47c8dea2af9b05a5f5a73cdee2f28d3ab
Maschkov Konstantin <maschkovc@yandex.ru>
2023-10-21 12:09:13 +0300

    немного валидации

--- src/components/searchCharacters/SearchCharacters.js
+++ src/components/searchCharacters/SearchCharacters.js
@@ -2,9 +2,20 @@
 import './searchCharacters.scss';
 
 import { Link } from 'react-router-dom/cjs/react-router-dom';
+import { useState } from 'react';
 
 
 const SearchCharacters = (props) => {
+
+    const [bord, setBord] = useState(false);
+
+    const cyrillicPattern = /^[\u0400-\u04FF]+$/;
+    const whitespacePAttern = /\s/g;
+
+    let border = { 'border': '4px solid black' }
+    if (bord) {
+        border = { 'border': '4px solid red' }
+    }
 
     return (
         <div className="search">
@@ -12,7 +23,15 @@ const SearchCharacters = (props) => {
                 <input className='search__searchInput'
                     type="text"
                     placeholder="Search character"
-                    onChange={(e) => { props.onCharNameSelected(e.target.value); }}
+                    style={border}
+                    onChange={(e) => {
+                        if (whitespacePAttern.test(e.target.value) || cyrillicPattern.test(e.target.value)) {
+                            setBord(true)
+                        } else {
+                            setBord(false)
+                            props.onCharNameSelected(e.target.value);
+                        }
+                    }}
                 />
                 <Link to="/foundСharacter" className='search__searchBtn button'>Search</Link>
             </form>
